e5ac178a7751873250c6fb3dcc65a494b0da9dbf
Sebastien Iniesta <sebastien.iniesta@gmail.com>
2019-10-14 14:15:12 +0200

    20191014 : Default Kiamo Samples : v7.x

---
+++ Samples/Default Kiamo Samples/v7.x/KiamoConnectorFileSample/config-sample.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'key' => 'fd36e8c9566b5d72341a451283a44f1fed65be0d',
+    'base' => '574c1d3995834b2412000029',
+    'fields' => [
+        'contactLastName',
+        'contactFirstName',
+        'Societe'
+    ]
+];
---
+++ Samples/Default Kiamo Samples/v7.x/KiamoConnectorMixedSample/config.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'connecteur1' => [
+        'name' => [
+            'en_US' => 'Connector One',
+            'fr_FR' => 'Connecteur Un',
+            'de_DE' => 'Connektor Ein'
+        ],
+        'key' => '36dd0ed6393147510828aab0d7c1d659b33442ac',
+        'base' => '57aaf8ab95834bf015000029',
+        'fields' => [
+            'contactLastName',
+            'contactFirstName',
+            'Societe'
+        ]
+    ],
+    'connecteur2' => [
+        'name' => [
+            'en_US' => 'Connector Two'
+        ],
+        'key' => '36dd0ed6393147510828aab0d7c1d659b33442ac',
+        'base' => '57aaf8ab95834bf015000029',
+        'fields' => [
+            'contactLastName',
+            'contactFirstName'
+        ]
+    ]
+];
